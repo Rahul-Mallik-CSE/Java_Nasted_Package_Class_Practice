6ee9bb8308cd44bd677643fe8f4680af164cb538
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2026-01-05 10:17:38 +0600

    Add Class1 and update Main class to create an instance of Class1

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,12 @@
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("Hello, World!");
+        
+        //Creating object using full name
+        package1.package2.package2_2.Class1 obj = new package1.package2.package2_2.Class1();
+
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        
+        
     }
 }
