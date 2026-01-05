3b404ce734a33bdbb6cf0fcac199f3603974a5a0
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2026-01-05 10:20:16 +0600

    Refactor Main class to simplify object creation for Class1 and Scanner

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,19 @@
-
+import java.util.Scanner;
+import package1.package2.package2_2.Class1;
 
 public class Main {
 
     public static void main(String[] args){
         
-        //Creating object using full name
-        package1.package2.package2_2.Class1 obj = new package1.package2.package2_2.Class1();
+        // //Creating object using full name
+        // package1.package2.package2_2.Class1 obj = new package1.package2.package2_2.Class1();
 
-        java.util.Scanner sc = new java.util.Scanner(System.in);
+        // java.util.Scanner sc = new java.util.Scanner(System.in);
         
+        //Creating object after importing the class
+        Scanner sc = new Scanner(System.in);
+
+        Class1 obj = new Class1();
         
     }
 }
