93dcac4addc809cde78bd06cb41b0297b8deaf8d
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2026-01-05 11:13:33 +0600

    Refactor Class1 and Class2 to improve main method structure and object instantiation

--- src/package1/package2/Class1.java
+++ src/package1/package2/Class1.java
@@ -1,9 +1,15 @@
 package package1.package2;
 
 public class Class1 {
-    
+
     public String str;
     public void fnc(){
         System.out.println("Class1 from package2");
+    }
+
+    public static void main(String[] args){
+        Class1 obj = new Class1();
+        obj.str = "Hello from Class1 in package2";
+        obj.fnc();
     }
 }
--- src/package1/package2/Class2.java
+++ src/package1/package2/Class2.java
@@ -1,8 +1,11 @@
 package package1.package2;
 
 public class Class2 {
-    Class1 obj = new Class1();
+    
+    public static void main(String[] args){
+        Class1 obj = new Class1();
 
-    obj.str = "Hello";
-    obj.fnc();
+        obj.str = "Hello";
+        obj.fnc();
+    }
 }
